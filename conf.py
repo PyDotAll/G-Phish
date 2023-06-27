306e933e9d7096ca87994152ece0805d464bde08
CarmZy <136038564+TranscriptAI@users.noreply.github.com>
2023-06-27 12:33:24 +0800

    Create conf.py
    Signed-off-by: CarmZy <136038564+TranscriptAI@users.noreply.github.com>

---
+++ conf.py
@@ -0,0 +1,17 @@
+# conf.py
+
+# -- Project information -----------------------------------------------------
+project = 'Your Project Name'
+# Replace 'Your Project Name' with the actual name of your project
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'sphinx.ext.autodoc',
+    # Add any other Sphinx extensions you want to use
+]
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+# Choose the theme you want to use for your documentation
+
+# Other configuration options can be added as needed
