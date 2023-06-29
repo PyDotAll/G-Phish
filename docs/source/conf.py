b08939d38e53d09bf251997c3ba648ce10d88da4
CarmZy <136038564+TranscriptAI@users.noreply.github.com>
2023-06-29 15:37:07 +0800

    Update conf.py
    Signed-off-by: CarmZy <136038564+TranscriptAI@users.noreply.github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'TranscribeAI'
+copyright = '2023, CarmZy'
+author = 'CarmZy'
 
 release = '0.1'
 version = '0.1.0'
